5880afb730fa5798a28e369d86c314505cce8c41
clement <clementmorere@gmail.com>
2026-01-25 18:37:03 +0100

    [ADD] menu principal avec boucle et saisie sécurisée

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,45 @@ public class Main {
    
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("=== Bienvenue dans le jeu Pokemon ===");
         System.out.println("Chargement du jeu...");
         System.out.print("Nom de votre équipe : ");
         String nomEquipe = scanner.nextLine();
 
+        int choix = -1;
+
         System.out.println("Equipe \"" + nomEquipe + "\" enregistrée.");
+
+        while (choix != 3) {
+
+            System.out.println();
+            System.out.println("=== MENU PRINCIPAL ===");
+            System.out.println("1 - Nouvelle partie");
+            System.out.println("2 - Charger une partie");
+            System.out.println("3 - Quitter");
+            System.out.print("Votre choix : ");
+            
+            if (scanner.hasNextInt()) {
+                choix = scanner.nextInt();
+            } else {
+                scanner.next();
+                choix = -1;
+            }
+            
+            switch (choix) {
+                case 1:
+                    System.out.println("Nouvelle partie (à venir)");
+                    break;
+                case 2:
+                    System.out.println("Chargement de la partie (à venir)");
+                    break;
+                case 3:
+                    System.out.println("Au revoir !");
+                    break;
+                default:
+                    System.out.println("Choix invalide.");
+            }
+        }
     }
 }
