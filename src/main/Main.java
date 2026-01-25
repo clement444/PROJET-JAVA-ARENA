345b1070be99cc4c76d64380ca0ef1c8798a5dc0
clement <clementmorere@gmail.com>
2026-01-25 17:40:10 +0100

    [ADD] préparation de la saisie utilisateur

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("=== Bienvenue dans le jeu Pokemon ===");
         System.out.println("Chargement du jeu...");
